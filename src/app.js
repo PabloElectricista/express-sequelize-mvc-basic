a86fa0c5d0a4e1ed4699eee36ceda2afd167e2a2
Pablo Spector <40707996+PabloElectricista@users.noreply.github.com>
2024-04-08 20:59:17 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import routes from './routes/index.js';
 import './config/db.js'
 
 const app = express();
-app.set('view engine', 'ejs')
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
